54706b51dc6f0654d4cdd0f8beb90cdd1f0f6241
rion <rionazareth@gmail.com>
2022-04-28 22:42:44 +0530

    code to convert eps files to jpg using pillow

--- convert_to_jpg.py
+++ convert_to_jpg.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import numpy as np
-for p in range(200,510,10):
+for p in range(0,20,1):
     p = np.float64(p)
-    img = Image.open(str(p)+'cic_10Mpc_1024g.eps')
+    img = Image.open('/home/rnazx/comp_phys_term_paper/eps_plots/'+str(p)+'cic_10Mpc_1024g.eps')
     rgb_img = img.convert('RGB')
-    rgb_img.save('/home/rnazx/eps_plots/jpg_plots/'+str(p)+'.jpg',quality = 95)
+    rgb_img.save('jpg_plots/'+str(p)+'.jpg',quality = 95)
