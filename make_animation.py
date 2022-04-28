cb026d9fd4204212f8f9a53129760f0dbfd76d9b
rion <rionazareth@gmail.com>
2022-04-28 22:42:21 +0530

    added animation using opencv

--- make_animation.py
+++ make_animation.py
@@ -1,8 +1,17 @@
-#!pip install ffmpeg-python
+#!pip install ffmpeg
 
+import cv2
 import numpy as np
 
-import ffmpeg
-stream = ffmpeg.input('jpg_plots/*.jpg', pattern_type='glob', framerate=5)
-stream = ffmpeg.output(stream, 'final.mp4')
-ffmpeg.run(stream)
+images = ['/home/rnazx/comp_phys_term_paper/jpg_plots/'+str(float(p))+'.jpg' for p in range(0,20,1)]
+print(images)
+
+frameSize = (1080, 1080)
+
+out = cv2.VideoWriter('movie.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 5, frameSize)
+
+for filename in images:
+    img = cv2.imread(filename)
+    out.write(img)
+
+out.release()
